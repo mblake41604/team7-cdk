ffcdf018b8158775d7f9dd65e8baf6908d043839
McKenna <mblakestuff@gmail.com>
2024-11-17 16:37:54 -0500

    trying

---
+++ test/team7-cdk.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const Team7Cdk = require('../lib/team7-cdk-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/team7-cdk-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new Team7Cdk.Team7CdkStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
